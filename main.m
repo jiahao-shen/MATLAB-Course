4371d9ef5fd7d5b3f0617e210784ccbad78c2d65
shenjiahao <1843781563@qq.com>
2020-04-13 05:03:59 +0800

    Daily update

--- main.m
+++ main.m
@@ -1,10 +1,39 @@
-n = 30;
-a = linspace(-pi / 2, pi / 2, n);
-b = linspace(0, 2 * pi, n);
-[a, b] = meshgrid(a, b);
-x = cos(b) .* cos(a);
-y = sin(b) .* cos(a);
-z = sin(a);
+[x, y, z] = peaks(50);
+
+subplot(3, 3, 1);
+peaks(50);
+title('peaks');
+
+subplot(3, 3, 2);
+plot3(x, y, z);
+title('plot3');
+
+subplot(3, 3, 3);
+stem3(x, y, z);
+title('stem3');
+
+subplot(3, 3, 4);
+mesh(x, y, z);
+title('mesh')
+
+subplot(3, 3, 5);
+meshc(x, y, z);
+title('meshc');
+
+subplot(3, 3, 6);
+meshz(x, y, z);
+title('meshz');
+
+subplot(3, 3, 7);
 surf(x, y, z);
-axis equal;
-print('sphere.png', '-dpng');
+title('surf');
+
+subplot(3, 3, 8);
+surfc(x, y, z);
+title('surfc');
+
+subplot(3, 3, 9);
+surfl(x, y, z);
+title('surfl');
+
+pause(10);
